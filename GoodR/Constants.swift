8c7fad15ba3fdb1a2019031d353a32ecd4cc5d8c
Sam Rabeeh <srabeeh@larci.com>
2016-02-26 17:42:04 -0700

    Update icons, fonts, styling

---
+++ GoodR/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GoodR
+//
+//  Created by Sam Rabeeh on 2016-02-24.
+//  Copyright © 2016 Sam Rabeeh - RCI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR:CGFloat = 157.0 / 255.0
