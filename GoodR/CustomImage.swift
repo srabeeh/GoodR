3c4cd79c9170442535dedecf9cb476e30274bad9
Sam Rabeeh <srabeeh@larci.com>
2016-03-02 07:17:43 -0700

    alamofir imagecache implemented. minor tweeks

---
+++ GoodR/CustomImage.swift
@@ -0,0 +1,13 @@
+//
+//  CustomImage.swift
+//  GoodR
+//
+//  Created by Sam Rabeeh on 2016-03-02.
+//  Copyright © 2016 Sam Rabeeh - RCI. All rights reserved.
+//
+
+import UIKit
+
+class CustomImage: UIImage {
+
+}
